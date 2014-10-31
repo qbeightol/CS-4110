7616056705d2d7f56bc84cec38c61c3e8ba244ce
ameyaacharya <apa52@cornell.edu>
2014-10-31 13:46:23 -0400

    starter code

--- hw/7/lam/eval.ml
+++ hw/7/lam/eval.ml
@@ -4,7 +4,9 @@ exception IllformedExpression
 
 (* evaluate CPS expression e in environment g *)
 let rec eval (g:cps_env) (c:cps_exp) : cps_val = 
-  failwith "Adriaan van Wijngaarden"
+  match c with
+  | CApp (exp, atom) -> failwith "hello"
+  | CAtom atom -> failwith "bonjour"
 
 (* evaluate CPS atom a in environment g *) 
 and eval_atom (g:cps_env) (a:cps_atom) : cps_val = 
