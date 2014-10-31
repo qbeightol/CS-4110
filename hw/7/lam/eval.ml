f1da2bc2160200e7f7dc111618e43839d4bb0e3b
ameyaacharya <apa52@cornell.edu>
2014-10-31 13:50:11 -0400

    Starter code for eval atom

--- hw/7/lam/eval.ml
+++ hw/7/lam/eval.ml
@@ -10,4 +10,17 @@ let rec eval (g:cps_env) (c:cps_exp) : cps_val =
 
 (* evaluate CPS atom a in environment g *) 
 and eval_atom (g:cps_env) (a:cps_atom) : cps_val = 
-  failwith "F. Lockwood Morris"
+  match a with
+  | CVar var -> failwith "hola"
+  | CLam (var, exp) -> failwith "salaam"
+  | CUnit -> failwith "namaste"
+  | CInt i -> failwith "shalom"
+  | CPlus (var1, var2) -> failwith "guten tag" 
+  | CPair (var1, var2) -> failwith "aloha" 
+  | CFst var -> failwith "ciao"
+  | CSnd var -> failwith "jambo"
+  | CTrue -> failwith "hei"
+  | CFalse -> failwith "goddag"
+  | CEq (var1, var2) -> failwith "hallo"
+  | CIf (b, branch_true, branch_false) -> failwith "ola"
+
